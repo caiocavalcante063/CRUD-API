af09b0e5656b91e03ffe093d0ea0f0a6b2ea8d6d
Caio César Amorim <caiocavalcante063@gmail.com>
2022-01-15 12:06:54 -0300

    cumpre requisito 02

--- index.js
+++ index.js
@@ -23,6 +23,19 @@ app.get('/talker', rescue(async (_req, res) => {
   res.status(200).json(JSON.parse(talkers));
 }));
 
+app.get('/talker/:id', rescue(async (req, res) => {
+  const { id } = req.params;
+  console.log(id);
+  const talkers = await fs.readFile('./talker.json', 'utf-8');
+  const talkersJson = JSON.parse(talkers);
+
+  const talker = talkersJson.find((t) => Number(t.id) === Number(id));
+
+  if (!talker) return res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
+
+  return res.status(200).json(talker);
+}));
+
 app.use((err, _req, res, _next) => {
   res.status(500).json({ error: `Erro: ${err.message}` });
 });
