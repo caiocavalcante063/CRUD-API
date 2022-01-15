be23bfb6ebd6dd530f9fc8efa4ccd2aabfebbb26
Caio César Amorim <caiocavalcante063@gmail.com>
2022-01-15 11:49:21 -0300

    cumpre requisito 01

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
+const rescue = require('express-rescue');
 const bodyParser = require('body-parser');
+const fs = require('fs').promises;
 
 const app = express();
 app.use(bodyParser.json());
@@ -14,4 +16,13 @@ app.get('/', (_request, response) => {
 
 app.listen(PORT, () => {
   console.log('Online');
+});
+
+app.get('/talker', rescue(async (_req, res) => {
+  const talkers = await fs.readFile('./talker.json', 'utf-8');
+  res.status(200).json(JSON.parse(talkers));
+}));
+
+app.use((err, _req, res, _next) => {
+  res.status(500).json({ error: `Erro: ${err.message}` });
 });
